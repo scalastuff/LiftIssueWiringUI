114cbe45b60bee863214c94a5d67d9a7245668f9
Ruud Diterwich <ruud@diterwich.com>
2011-11-01 14:13:18 +0100

    WiringUI + RequestVar issue sample

--- src/main/scala/test/snippet/ColorSnippet.scala
+++ src/main/scala/test/snippet/ColorSnippet.scala
@@ -1,29 +1,35 @@
 package test.snippet
 
-import net.liftweb.util.Helpers._
+import xml.Text
 import net.liftweb.http.RequestVar
-import net.liftweb.util.ValueCell
+import net.liftweb.http.SessionVar
+import net.liftweb.http.SHtml
 import net.liftweb.http.WiringUI
+import net.liftweb.http.js.JsCmds.Noop
 import net.liftweb.util.Helpers
+import net.liftweb.util.Helpers._
+import net.liftweb.util.ValueCell
 
 
 object ColorSnippet {
-  val colors = "red" :: "green" :: "blue" :: Nil
-  object Current extends RequestVar[ValueCell[String]](ValueCell(colors(0)))
-
-  def render1 = "tbody *" #> {
-    "tr" #> colors.map { color =>
-      "tr [class]" #> (if (color == Current.currentValue._1) "selected" else "") &
-      "@color" #> color
-    }
-  }
+  val allColors = "red" :: "green" :: "blue" :: "purple" :: "yellow" :: "orange" :: Nil
   
-  def render2 = "tbody *" #> {
-    "tr" #> colors.map { color =>
-      WiringUI(Current) { currentColor => 
-        "tr [class]" #> (if (color == Current.currentValue._1) "selected" else "") &
-        "@color" #> color
+  object Colors extends RequestVar[ValueCell[Seq[String]]](ValueCell(allColors))
+  object Current extends RequestVar[ValueCell[String]](ValueCell(allColors(0)))
+
+  def search = "input" #> SHtml.ajaxText("", false, { (value : String) =>
+    val colors = if (value.trim.isEmpty) allColors else allColors.filter(_.contains(value.trim))
+    Colors.get.atomicUpdate(_ => colors)
+    Noop
+  })    
+  
+  def table = WiringUI(Colors) { colors => 
+    "tbody *" #> {
+      "tr" #> colors.map { color =>
+        "@color" #> SHtml.a(() => { Current.is.atomicUpdate(_ => color); Noop }, Text(color))
       }
     }
   }
+
+  def currentColor = "*" #> WiringUI.asText(Current)
 }
